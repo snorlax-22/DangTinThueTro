95ff026fee1ad3c797f474c6b655c3cbfa903321
snorlax-22 <77501426+snorlax-22@users.noreply.github.com>
2021-06-16 22:52:25 +0700

    Update DangTinThueTro.java

--- src/dangtinthuetro/DangTinThueTro.java
+++ src/dangtinthuetro/DangTinThueTro.java
@@ -8,10 +8,5 @@ public class DangTinThueTro extends JFrame{
         
     public static void main(String[] args) throws IOException {
         new  MainScreen().setVisible(true);
-<<<<<<< HEAD
-        //123123
-=======
-       //pleoads
->>>>>>> 0e37a85168ac1d76f2d5c668d6963c6010b8bc5c
     }
 }
